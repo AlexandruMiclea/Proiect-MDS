6203d40e60580f78d44aecd11a60e4b3c6fc78ac
Tudor Chițu <115002435+tedi11@users.noreply.github.com>
2024-06-12 03:09:10 +0300

    itinerariu, harta, calendar

--- src/app/(tabs)/itinerary/itineraryPage.tsx
+++ src/app/(tabs)/itinerary/itineraryPage.tsx
@@ -31,7 +31,6 @@ const ItineraryPage = () => {
     const [cityLon, setCityLon] = useState<string | null>(null);
     const [currentTemperature, setCurrentTemperature] = useState(null);
 
-    // Get the current temperature of the city
     useEffect(() => {
         (async () => {
             // Request permission to access calendars
@@ -66,9 +65,10 @@ const ItineraryPage = () => {
             return answer;
         };
 
-        // Get the coordinates of the city
-        const itineraryDataCall = async () => {
-            let query: LocationQuery = {
+        let locationDataArray: sampleLocationData[] = [];
+
+        for (var searchValue of searchValueList) {
+            const query: LocationQuery = {
                 key: apiKey,
                 searchQuery: searchValue,
                 category: "attractions",
@@ -78,84 +78,54 @@ const ItineraryPage = () => {
             };
             const ans = await getAttractions({ reqParams: query });
 
-            for (var searchValue of searchValueList) {
-                query.searchQuery = searchValue;
-                const ans = await getAttractions({ reqParams: query });
-
-                for (var location of ans.data.slice(0, 3)) {
-                    let imgQuery: PhotoQuery = {
-                        key: apiKey,
-                        language: "en",
-                        limit: 3
-                    };
-                    const photoAns = await getPhotos({ locationId: location.location_id, reqParams: imgQuery });
-                    var urls: Array<{ imageUrl: string | null }> = [];
-                    photoAns.data.forEach(x => urls.push({ imageUrl: x.images.large.url }));
-                    var newLocation: sampleLocationData = {
-                        title: location.name, // Now matches the updated type definition
-                        address: location.address_obj.address_string,
-                        description: "",
-                        images : urls// Now matches the updated type definition
-                    };
-                    setLocationData(locationData => [...locationData, newLocation]);
-                }
+            for (var location of ans.data.slice(0, 3)) {
+                const imgQuery: PhotoQuery = {
+                    key: apiKey,
+                    language: "en",
+                    limit: 3
+                };
+                const photoAns = await getPhotos({ locationId: location.location_id, reqParams: imgQuery });
+                const urls: Array<{ imageUrl: string | null }> = [];
+                photoAns.data.forEach(x => urls.push({ imageUrl: x.images.large.url }));
+                const newLocation: sampleLocationData = {
+                    title: location.name,
+                    address: location.address_obj.address_string,
+                    description: "",
+                    images : urls
+                };
+                locationDataArray.push(newLocation);
             }
-            setLoaded(true);
+        }
+
+        setLocationData(locationDataArray);
+        setLoaded(true);
+    };
+
+    /**
+     * Handles the save button click event.
+     * @param event - The event object.
+     */
+    async function handleSave(event: GestureResponderEvent): Promise<void> {
+        setButtonVisible(false);
+        console.log("Save button clicked");
+        console.log(session?.user.id);
+        const itineraryData = {
+            profile_id: session?.user.id,
+            city: params.city,
+            country: params.country,
+            start_date: params.startDate,
+            final_date: params.endDate,
+            budget: params.budget
         };
-
-        itineraryDataCall();
-    }, [setLoaded, setLocationData]);
-    
-    if (!loaded) {
-        return (
-            <View style={styles.loadingScreen}>
-                <ActivityIndicator size="large" color="tint" />
-            </View>
-        );
-    } else {
-        async function handleSave(event: GestureResponderEvent): Promise<void> {
-            setButtonVisible(false);
-            console.log("Save button clicked");
-            console.log(session?.user.id);
-            const itineraryData = {
-                profile_id: session?.user.id,
-                city: params.city,
-                country: params.country,
-                start_date: params.startDate,
-                final_date: params.endDate,
-                budget: params.budget
-            };
-
 
         const { data: newItinerary, error: itineraryError } = await supabase.from('itineraries').insert([itineraryData]).select();
 
+        if (itineraryError) {
+            console.error("Error saving itinerary:", itineraryError);
+            return;
+        }
 
-            // If there was an error saving the itinerary, log it and return
-            if (itineraryError) {
-                console.error("Error saving itinerary:", itineraryError);
-                return;
-            }
-
-            // Get the ID of the new itinerary
-            const newItineraryId = newItinerary[0].id;
-
-            const locationPromises = locationData.map(loc => {
-                const locationData = {
-                    id_itinerar: newItineraryId,
-                    title: loc.title,
-                    description: loc.description,
-                    address: loc.address,
-                    photo1: loc.images[0]?.imageUrl || null,
-                    photo2: loc.images[1]?.imageUrl || null,
-                    photo3: loc.images[2]?.imageUrl || null,
-                    photo4: loc.images[3]?.imageUrl || null,
-                    photo5: loc.images[4]?.imageUrl || null
-                };
-
-                return supabase.from('locations').insert([locationData]);
-            });
-            // Wait for all locations to be saved
-            const locationResults = await Promise.all(locationPromises);
+        const newItineraryId = newItinerary[0].id;
 
         const defaultCalendarSource =
             Platform.OS === 'ios'
@@ -215,9 +185,12 @@ const ItineraryPage = () => {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 {buttonVisible && (
-                <Pressable style={{...styles.button_save, backgroundColor: mainColor}} onPress={handleSave}>
-                    <Text style={styles.buttonText}>Save</Text>
-                </Pressable>
+                    <Pressable 
+                        style={{...styles.button_save, backgroundColor: mainColor}} 
+                        onPress={handleSave}
+                    >
+                        <Text style={styles.buttonText}>Save</Text>
+                    </Pressable>
                 )}
                 <Text style={styles.label}>{params.city}, {params.country}</Text>
                 <Text style={styles.label}>{params.startDate} - {params.endDate}</Text>
