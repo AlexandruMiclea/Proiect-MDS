ba12ec445eb34fe7bb7be3248bbb4bafa76ea111
Tudor Chițu <115002435+tedi11@users.noreply.github.com>
2024-06-12 04:34:19 +0300

    Am terminat si testat chestiile finale

--- src/app/(tabs)/itinerary/itineraryPage.tsx
+++ src/app/(tabs)/itinerary/itineraryPage.tsx
@@ -4,7 +4,7 @@ import countriesCitiesJson from '@assets/data/countriesInfo.json';
 import { LocationQuery, LocationObj, PhotoQuery, sampleLocationData } from '@/types';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Pressable, View, Text, ActivityIndicator, StyleSheet, GestureResponderEvent, Platform } from 'react-native';
+import { Pressable, View, Text, ActivityIndicator, StyleSheet, GestureResponderEvent, Platform, Alert } from 'react-native';
 import LocationList from './locationList';
 import { supabase } from '@/lib/supabase';
 import { useAuth } from "@/app/providers/AuthProvider";
@@ -25,7 +25,7 @@ const ItineraryPage = () => {
     const [loaded, setLoaded] = useState(false);
     const [locationData, setLocationData] = useState<sampleLocationData[]>([]);
     const { session } = useAuth();
-    const searchValueList = ["art gallery", "museum", "monument"]; 
+    const searchValueList = [["stadium", "attractions"], ["museum", "attractions"], ["restaurant", "restaurants"]];
     const [buttonVisible, setButtonVisible] = useState(true);
     const colorScheme = useColorScheme();
     const mainColor = Colors[colorScheme ?? 'light'].tint;
@@ -33,6 +33,7 @@ const ItineraryPage = () => {
     const [cityLat, setCityLat] = useState<string | null>(null);
     const [cityLon, setCityLon] = useState<string | null>(null);
     const [isCelsiusActive, setIsCelsiusActive] = useState(true);
+    let locationDataArray: sampleLocationData[] = [];
 
     const toggleTemperatureUnit = () => {
         setIsCelsiusActive(!isCelsiusActive);
@@ -75,6 +76,26 @@ const ItineraryPage = () => {
      * Loads the data for the itinerary page.
      */
     const loadData = async () => {
+
+        if (!session?.user) throw new Error("No user on the session!");
+        
+        const { data, error, status } = await supabase
+            .from("profiles")
+            .select(
+            `preference_sports, preference_food, preference_arts, preference_itinerary_complexity`
+            )
+            .eq("id", session?.user.id)
+            .single();
+        if (error && status !== 406) {
+            console.log(error);
+            throw error;
+        }
+    
+        let PreferenceSport = data?.preference_sports;
+        let PreferenceArts = data?.preference_arts;
+        let PreferenceFood = data?.preference_food;
+        let ItineraryComplexity = data?.preference_itinerary_complexity;
+
         const getCoordinates = () => {
             let answer = "";
             if (params.country != undefined){
@@ -91,43 +112,159 @@ const ItineraryPage = () => {
             return answer;
         };
 
-        let locationDataArray: sampleLocationData[] = [];
+        
 
-        for (var searchValue of searchValueList) {
-            const query: LocationQuery = {
-                key: apiKey,
-                searchQuery: searchValue,
-                category: "attractions",
-                latLong: getCoordinates(),
-                radius: 20,
-                radiusUnit: "km"
-            };
-            const ans = await getAttractions({ reqParams: query });
+        const query: LocationQuery = {
+            key: apiKey,
+            searchQuery: "",
+            category: "",
+            latLong: getCoordinates(),
+            radius: 20,
+            radiusUnit: "km"
+        };
 
-            for (var location of ans.data.slice(0, 3)) {
-                const imgQuery: PhotoQuery = {
-                    key: apiKey,
-                    language: "en",
-                    limit: 3
-                };
-                const photoAns = await getPhotos({ locationId: location.location_id, reqParams: imgQuery });
-                const urls: Array<{ imageUrl: string | null }> = [];
-                photoAns.data.forEach(x => urls.push({ imageUrl: x.images.large.url }));
-                const newLocation: sampleLocationData = {
-                    title: location.name,
-                    address: location.address_obj.address_string,
-                    description: "",
-                    images : urls
-                };
 
-                const isTitleExists = locationDataArray.some(existingLocation => existingLocation.title === newLocation.title);
 
-                if (!isTitleExists) {
-                    locationDataArray.push(newLocation);
+            let updatedLocationData = [...locationData]; //local copy of the current state and used for processing duplicates to avoid issues with async state updates (locationData is state managed with useState) 
+            var idxList = 0; // 0 to 2, 0 -> sports, 1 -> attractions, 2 -> food
+            var avgPrefs = ((1 + PreferenceSport) + (1 + PreferenceArts) + (1 + PreferenceFood)) / 3;
+            console.log("sumprefs: " + avgPrefs);
+            var numberOfLocations = 0;
+            var targetLocationNo = 1 + (2 * ItineraryComplexity);
+            console.log("tlno: " + targetLocationNo);
+            for (var searchValue of searchValueList){
+                switch(idxList) {
+                    case 0:
+                        numberOfLocations = Math.ceil((1 + PreferenceSport) / (10 - targetLocationNo));
+                        console.log(0 + " " + numberOfLocations);
+                        break;
+                    case 1:
+                        numberOfLocations = Math.ceil((1 + PreferenceArts) / (10 - targetLocationNo));
+                        console.log(1 + " " + numberOfLocations);
+                        break;
+                    case 2:
+                        numberOfLocations = Math.ceil((1 + PreferenceFood) / (10 - targetLocationNo));
+                        console.log(2 + " " + numberOfLocations);
+                        break;
+                    default:
+                        break;
                 }
-
+                query.searchQuery = searchValue[0];
+                query.category = searchValue[1];
+                const ans = await getAttractions({reqParams : query});
+                console.log("now number of locations")
+                // TODO for now I get 3 of each. We should add more based on the sliders of the authenticated user
+                for (var location of ans.data){
+                    if (numberOfLocations === 0) {
+                        break;
+                    }
+                    console.log(location.name);
+                    if (updatedLocationData.some(loc => loc.title === location.name)) {
+                        console.log("Skipping location: ${location.name} because it's a duplicate");
+                        continue; 
+                    }
+                    numberOfLocations -= 1;
+                    console.log(numberOfLocations);
+                    let imgQuery: PhotoQuery = {
+                        key: apiKey,
+                        language: "en",
+                        limit: 3
+                    }
+                    const photoAns = await getPhotos({locationId: location.location_id, reqParams: imgQuery});
+                    var urls: [{imageUrl: string}?] = [];
+                    photoAns.data.forEach(x => urls.push({imageUrl: x.images.large.url}));
+                    var newLocation : sampleLocationData = {
+                        title: location.name, 
+                        address: location.address_obj.address_string, 
+                        description: "", 
+                        images: urls
+                    }
+                    updatedLocationData.push(newLocation);
+                }
+                idxList += 1;
             }
-        }
+            setLocationData(updatedLocationData);
+            // TODO for each location get requests for the picture and description
+            // TODO we can also get cost
+            setLoaded(true);
+        };
+
+
+    if (!loaded) {
+        return (
+            <View style={styles.loadingScreen}>
+                <ActivityIndicator size="large" color="tint" />
+            </View>
+        );
+    } else {
+        async function handleSave(event: GestureResponderEvent): Promise<void> {
+            setButtonVisible(false);
+            console.log("Save button clicked");
+            console.log(session?.user.id);
+            const itineraryData = {
+                profile_id: session?.user.id,
+                city: params.city,
+                country: params.country,
+                start_date: params.startDate,
+                final_date: params.endDate,
+                budget: params.budget
+            };
+
+            const { data: newItinerary, error: itineraryError } = await supabase.from('itineraries').insert([itineraryData]).select();
+
+            if (itineraryError) {
+                console.error("Error saving itinerary:", itineraryError);
+                return;
+            }
+    
+            const newItineraryId = newItinerary[0].id;
+    
+            const defaultCalendarSource =
+                Platform.OS === 'ios'
+                ? await getDefaultCalendarSource()
+                : { isLocalAccount: true, name: 'Expo Calendar' };
+                const newCalendarID = await Calendar.createCalendarAsync({
+                title: 'Trip Itinerary',
+                color: mainColor,
+                entityType: Calendar.EntityTypes.EVENT,
+                sourceId: defaultCalendarSource.id,
+                source: defaultCalendarSource,
+                name: 'Trip Itinerary',
+                ownerAccount: 'personal',
+                accessLevel: Calendar.CalendarAccessLevel.OWNER,
+            });
+            console.log(`Your new calendar ID is: ${newCalendarID}`);
+    
+            const startDateParts = params.startDate.split('.');
+            const today = new Date(Number(startDateParts[2]), Number(startDateParts[1]) - 1, Number(startDateParts[0]));
+            const endDateParts = params.endDate.split('.');
+            const tomorrow = new Date(Number(endDateParts[2]), Number(endDateParts[1]) - 1, Number(endDateParts[0]));
+    
+            const details = {
+                title: params.city,
+                startDate: today,
+                endDate: tomorrow,
+                notes: `Itinerary for ${params.city}, ${params.country}`,
+                location: ''
+            };
+    
+            const eventId = await Calendar.createEventAsync(newCalendarID, details);
+    
+            // Save the event ID in the database
+            const locationData = {
+                id_itinerar: newItineraryId,
+                title: params.city,
+                description: `Itinerary for ${params.city}, ${params.country}`,
+                address: '',
+            };
+    
+            const { error: locationError } = await supabase.from('locations').insert([locationData]);
+    
+            if (locationError) {
+                console.error("Error saving location:", locationError);
+            }
+    
+            console.log("Data saved successfully");
 
         const fetchCurrentWeather = async () => {
             try {
@@ -149,87 +286,7 @@ const ItineraryPage = () => {
         setLoaded(true);
     };
 
-    /**
-     * Handles the save button click event.
-     * @param event - The event object.
-     */
-    async function handleSave(event: GestureResponderEvent): Promise<void> {
-        setButtonVisible(false);
-        console.log("Save button clicked");
-        console.log(session?.user.id);
-        const itineraryData = {
-            profile_id: session?.user.id,
-            city: params.city,
-            country: params.country,
-            start_date: params.startDate,
-            final_date: params.endDate,
-            budget: params.budget
-        };
 
-        const { data: newItinerary, error: itineraryError } = await supabase.from('itineraries').insert([itineraryData]).select();
-
-        if (itineraryError) {
-            console.error("Error saving itinerary:", itineraryError);
-            return;
-        }
-
-        const newItineraryId = newItinerary[0].id;
-
-        const defaultCalendarSource =
-            Platform.OS === 'ios'
-            ? await getDefaultCalendarSource()
-            : { isLocalAccount: true, name: 'Expo Calendar' };
-            const newCalendarID = await Calendar.createCalendarAsync({
-            title: 'Trip Itinerary',
-            color: mainColor,
-            entityType: Calendar.EntityTypes.EVENT,
-            sourceId: defaultCalendarSource.id,
-            source: defaultCalendarSource,
-            name: 'Trip Itinerary',
-            ownerAccount: 'personal',
-            accessLevel: Calendar.CalendarAccessLevel.OWNER,
-        });
-        console.log(`Your new calendar ID is: ${newCalendarID}`);
-
-        const startDateParts = params.startDate.split('.');
-        const today = new Date(Number(startDateParts[2]), Number(startDateParts[1]) - 1, Number(startDateParts[0]));
-        const endDateParts = params.endDate.split('.');
-        const tomorrow = new Date(Number(endDateParts[2]), Number(endDateParts[1]) - 1, Number(endDateParts[0]));
-
-        const details = {
-            title: params.city,
-            startDate: today,
-            endDate: tomorrow,
-            notes: `Itinerary for ${params.city}, ${params.country}`,
-            location: ''
-        };
-
-        const eventId = await Calendar.createEventAsync(newCalendarID, details);
-
-        // Save the event ID in the database
-        const locationData = {
-            id_itinerar: newItineraryId,
-            title: params.city,
-            description: `Itinerary for ${params.city}, ${params.country}`,
-            address: '',
-        };
-
-        const { error: locationError } = await supabase.from('locations').insert([locationData]);
-
-        if (locationError) {
-            console.error("Error saving location:", locationError);
-        }
-
-        console.log("Data saved successfully");
-    }
-
-    if (!loaded) {
-        return (
-            <View style={styles.loadingScreen}>
-                <ActivityIndicator size="large" color="tint" />
-            </View>
-        );
-    } else {
         return (
             <ScrollView>
             <View style={styles.detailsContainer}>
@@ -254,7 +311,7 @@ const ItineraryPage = () => {
             <View style={{ alignItems: 'center', justifyContent: "center" }}>
                 {buttonVisible && (
                 <Pressable 
-                    style={{...styles.button_save, backgroundColor: mainColor, alignItems: 'center', justifyContent: "center"}} 
+                    style={{...styles.button_save, backgroundColor: mainColor}} 
                     onPress={handleSave}
                 >
                     <Text style={styles.buttonText}>Save</Text>
@@ -274,6 +331,7 @@ const ItineraryPage = () => {
 export default ItineraryPage;
 
 const styles = StyleSheet.create({
+    
     detailsContainer:{
         flexDirection: 'row',
         justifyContent: 'space-between',
